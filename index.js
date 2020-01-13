f205d0a7a19487d4b9a0dce0b6e5cdf9899217b3
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-01-13 11:16:21 -0800

    Commit 3

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const Hubs = require('./data/hubs-model.js'); // our Hubs database library
 
 const server = express();
 
+// middleware: teaches express new things
+server.use(express.json()); // needed to parse JSON
+
 // routes or endpoints
 
 // GET to "/"
@@ -20,19 +23,53 @@ server.get('/api/hubs', function(req, res) {
     // read the data from the database (Hubs)
     Hubs.find() // returns a promise
         .then(hubs => {
+            console.log('Hubs', hubs);
             res.status(200).json(hubs);
         })
         .catch(err => {
             console.log(err);
             // handle the error
-            res.status(500).json({ errorMessage: "Sorry, we ran into an error getting the list of hubs."})
-        })
-})
+            res.status(500).json({ 
+                errorMessage: "Sorry, we ran into an error getting the list of hubs."
+            });
+        });
+});
 
 
 // create a Hub
+server.post('/api/hubs', (req, res) => {
+    const hubData = req.body;
+    // never trust the client, validate the data. for now we trust the data for the demo
+    Hubs.add(hubData)
+      .then(hub => {
+        res.status(201).json(hub);
+      })
+      .catch(error => {
+        console.log(error);
+        // handle the error
+        res.status(500).json({
+          errorMessage: 'sorry, we ran into an error creating the hub',
+        });
+    });
+});
 
 // delete a Hub
+server.delete('/api/hubs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Hubs.remove(id)
+        .then(deleted => {
+            res.status(200).json(deleted);
+        })
+        .catch(error => {
+            console.log(error);
+            // handle the error
+            res.status(500).json({
+              errorMessage: 'sorry, we ran into an error removing the hub',
+            });
+        });
+})
+
 
 // update a Hub
 
